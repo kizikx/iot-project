bc3298a52615f70fee7abb5a356dbb91637a7432
François Martin <contact@francois-martin.com>
2020-05-18 03:08:09 +0200

    This was a bad idea

--- iot-ui/src/views/DeviceInfos.vue
+++ iot-ui/src/views/DeviceInfos.vue
@@ -14,24 +14,6 @@
           <h5>Première connexion</h5>
           <p>{{ subscribeDate }}</p>
         </v-ons-card>
-        <line-chart
-          v-if="loaded"
-          :chartdata="chartData"
-          :options="options"
-          style="width: 95%"
-        ></line-chart>
-        <line-chart
-          v-if="loaded"
-          :chartdata="chartData"
-          :options="options"
-          style="width: 95%"
-        ></line-chart>
-        <line-chart
-          v-if="loaded"
-          :chartdata="chartData"
-          :options="options"
-          style="width: 95%"
-        ></line-chart>
       </v-ons-col>
       <v-ons-col width="25%" class="hide-md"></v-ons-col>
     </v-ons-row>
@@ -39,31 +21,9 @@
 </template>
 
 <script>
-import LineChart from '@/components/LineChart.vue';
-
 export default {
-  components: {
-    LineChart,
-  },
   data() {
     return {
-      loaded: true,
-      chartData: {
-        labels: ['January', 'February', 'March', 'April'],
-        datasets: [
-          {
-            backgroundColor: '#f87979',
-            data: [40, 20, 35, 12],
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        legend: {
-          display: false,
-        },
-      },
     };
   },
   computed: {
